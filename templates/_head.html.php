99287306561cbf22e2966770c1253b753eae2d93
Mikołaj Pich <m.pich@outlook.com>
2016-06-05 18:20:05 +0200

    Add link to logout

--- templates/_head.html.php
+++ templates/_head.html.php
@@ -74,7 +74,7 @@
 									<a href="#" class="btn btn-default btn-flat">Profile</a>
 								</div>
 								<div class="pull-right">
-									<a href="#" class="btn btn-default btn-flat">Sign out</a>
+									<a href="<?=DIR;?>/user/logout" class="btn btn-default btn-flat">Sign out</a>
 								</div>
 							</li>
 						</ul>
